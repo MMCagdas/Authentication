76455185b7232d57b3771652554fe03863483eab
LightbringerAF <mustafacagdas07@gmail.com>
2025-04-03 00:56:52 +0300

    cors update 2

--- src/app.ts
+++ src/app.ts
@@ -55,6 +55,15 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// Tüm yanıtlara CORS başlıklarını ekleyen middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://todo-b4cad.web.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 app.use(express.json());
 
 // Test endpoint'i
